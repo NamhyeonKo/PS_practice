e7226890f6dcfa369e724c55b57c499f009c2c98
NamHyun <glucosamin7@naver.com>
2023-03-26 19:51:55 +0900

    ㅠㅠ

--- 이런저런문제/37-6588.py
+++ 이런저런문제/37-6588.py
@@ -18,7 +18,7 @@ while True:
     if n == 0:
         break
 
-    for i in range(3,(n//2)+1,2):   # 이 반복문이 함정인데, 어차피 홀수만 판단하므로 3부터 2씩 더하면 반복문을 2배 빨리 돌릴 수 있다.
+    for i in range(3,(n//2)+1,2):   # 이 반복문이 함정인데, 어차피 홀수만 판단하므로 3부터 2씩 더하면 반복문을 2배 빨리 돌릴 수 있다.(는 거를 늦게 깨달음;;)
         if isprime[i] and isprime[n-i]:
             print('%d = %d + %d' % (n,i,n-i))
             break
